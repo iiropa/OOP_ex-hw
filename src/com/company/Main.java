acbad6bea370f56afd3b63f76c3194ada355c7d8
iiropa <iiropaajanen2004@gmail.com>
2021-09-22 11:36:04 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Product[] products = new Product [2];
+    products [0] = new Product (1, 25.6);
+    products [1] = new Product (2, 102.1);
+
+    for (Product p : products)
+        p.print();
+    }
+}
+class Product{
+    int id;
+    double price;
+
+    Product (int id, double price){
+        this.id = id;
+        this.price = price;
+    }
+    void print () {
+        System.out.println("id : " + id + " price : " + price);
+    }
+
+}
